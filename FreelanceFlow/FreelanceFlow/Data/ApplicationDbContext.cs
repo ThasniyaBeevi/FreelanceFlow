5995229d2f03d902c7f4fd63d899aa61ed9697ea
ThasniyaBeevi <thasniyabeevit@gmail.com>
2026-08-07 21:14:27 +0530

    Initial Migration and Client table created

---
+++ FreelanceFlow/FreelanceFlow/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using FreelanceFlow.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreelanceFlow.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients { get; set; }
+    }
+}
---
+++ FreelanceFlow/FreelanceFlow/Model/Client.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreelanceFlow.Model
+{
+    public class Client
+    {
+        [Key]
+        public long Id {  get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CompanyName { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
