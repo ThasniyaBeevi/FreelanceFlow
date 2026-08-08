a9fb6082a0416f2f785e281948d46743c45842e2
ThasniyaBeevi <thasniyabeevit@gmail.com>
2026-08-08 22:42:51 +0530

    Client crud apis

---
+++ FreelanceFlow/FreelanceFlow/Controllers/ClientsController.cs
@@ -0,0 +1,91 @@
+﻿using System.Threading.Tasks;
+using FreelanceFlow.Data;
+using FreelanceFlow.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreelanceFlow.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ClientsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Client>>> Clients()
+        {
+            var clients = await _dbContext.Clients.ToListAsync();
+            return Ok(clients);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Client>> Clients(long id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Id required, Invalid Params");
+            }
+            var client = await _dbContext.Clients.FindAsync(id);
+            if (client == null)
+            {
+                return NotFound("Client not found.");
+            }
+            return Ok(client);
+        }
+        [HttpPost]
+        public async Task<ActionResult<Client>> Clients(Client client)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            client.CreatedAt = DateTime.UtcNow;
+            await _dbContext.Clients.AddAsync(client);
+            await _dbContext.SaveChangesAsync();
+            return Ok(client);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Client>> Clients(long id , Client client)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Id required, Invalid Params");
+            }
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var data = await _dbContext.Clients.FindAsync(id);
+            if (data == null)
+            {
+                return NotFound("Client not found.");
+            }
+            data.Name = client.Name;
+            data.Email = client.Email;
+            data.PhoneNumber = client.PhoneNumber;
+            data.CompanyName = client.CompanyName;
+            await _dbContext.SaveChangesAsync();
+            return Ok(client);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteClient(long id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest("Id required, Invalid Params");
+            }
+            var data = await _dbContext.Clients.FindAsync(id);
+            if (data == null)
+            {
+                return NotFound("Client not found.");
+            }
+            _dbContext.Clients.Remove(data);
+            await _dbContext.SaveChangesAsync();
+            return Ok("client deleted successfully");
+        }
+    }
+}
--- FreelanceFlow/FreelanceFlow/Model/Client.cs
+++ FreelanceFlow/FreelanceFlow/Model/Client.cs
@@ -8,6 +8,8 @@ namespace FreelanceFlow.Model
         public long Id {  get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string CompanyName { get; set; }
