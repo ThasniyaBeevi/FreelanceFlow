470ee6c17ab1f4f7b013798448ef8455925357c3
Thasniya Beevi T <94682839+ThasniyaBeevi@users.noreply.github.com>
2026-08-17 15:25:00 +0530

    feat: wire dashboard, clients and projects routes

--- FreelanceFlow/FreelanceFlow.Client/src/app/app.routes.ts
+++ FreelanceFlow/FreelanceFlow.Client/src/app/app.routes.ts
@@ -1,19 +1,15 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './components/dashboard/dashboard';
 import { ClientList } from './components/client-list/client-list';
 import { ClientForm } from './components/client-form/client-form';
+import { ProjectList } from './components/project-list/project-list';
+import { ProjectForm } from './components/project-form/project-form';
 
 export const routes: Routes = [
-  {
-    path: 'clients',
-    component: ClientList
-  },
-  {
-    path: 'clients/new',
-    component: ClientForm
-  },
-  {
-    path: '',
-    redirectTo: 'clients',
-    pathMatch: 'full'
-  }
+  { path: '', component: Dashboard },
+  { path: 'clients', component: ClientList },
+  { path: 'clients/new', component: ClientForm },
+  { path: 'projects', component: ProjectList },
+  { path: 'projects/new', component: ProjectForm },
+  { path: '**', redirectTo: '' }
 ];
