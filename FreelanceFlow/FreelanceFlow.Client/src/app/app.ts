1bd13e721e1e1c24f37cfade08178966f6b23870
Thasniya Beevi T <94682839+ThasniyaBeevi@users.noreply.github.com>
2026-08-17 15:24:04 +0530

    feat: create application shell navigation

--- FreelanceFlow/FreelanceFlow.Client/src/app/app.ts
+++ FreelanceFlow/FreelanceFlow.Client/src/app/app.ts
@@ -1,12 +1,5 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
-})
-export class App {
-  protected readonly title = signal('FreelanceFlow.Client');
-}
+@Component({ selector: 'app-root', imports: [RouterOutlet, RouterLink, RouterLinkActive], templateUrl: './app.html', styleUrl: './app.css' })
+export class App {}
