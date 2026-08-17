c81462f42e51af734a13346dbd843bc68755e2dc
Thasniya Beevi T <94682839+ThasniyaBeevi@users.noreply.github.com>
2026-08-17 15:25:19 +0530

    polish: improve client list component

--- FreelanceFlow/FreelanceFlow.Client/src/app/components/client-list/client-list.ts
+++ FreelanceFlow/FreelanceFlow.Client/src/app/components/client-list/client-list.ts
@@ -1,29 +1,10 @@
 import { Component, OnInit, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Client } from '../../models/client';
 import { ClientService } from '../../services/client.service';
 
-@Component({
-  selector: 'app-client-list',
-  standalone: true,
-  imports: [],
-  templateUrl: './client-list.html',
-  styleUrl: './client-list.css'
-})
+@Component({selector:'app-client-list',standalone:true,imports:[RouterLink],templateUrl:'./client-list.html',styleUrl:'./client-list.css'})
 export class ClientList implements OnInit {
-
-  private clientService = inject(ClientService);
-
-  clients: Client[] = [];
-
-  ngOnInit(): void {
-    this.clientService.getClients().subscribe({
-      next: (data) => {
-        this.clients = data;
-        console.log('Clients:', this.clients);
-      },
-      error: (error) => {
-        console.error('Error loading clients:', error);
-      }
-    });
-  }
+ private clientService=inject(ClientService); clients:Client[]=[]; loading=true; error='';
+ ngOnInit(){this.clientService.getClients().subscribe({next:data=>{this.clients=data;this.loading=false},error:()=>{this.error='Unable to load clients';this.loading=false}})}
 }
